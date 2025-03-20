cb10ca814d30a3cc08a50c6fd94486aada989e51
kelvinguchu <kelvinguchu5@gmail.com>
2025-03-20 19:16:55 +0300

    Update package dependencies, enhance global styles, and integrate Navbar component into layout

---
+++ components/layout/navbar.tsx
@@ -0,0 +1,11 @@
+"use client"
+import Image from "next/image"
+
+
+export default function Navbar() {
+    return (
+        <div>
+            <Image src="/logo.png" alt="logo" width={100} height={100} />
+        </div>
+    )
+}
